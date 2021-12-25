f7e0d02fcd090517202b8d2df84126cfd90f3bf5
phoenix <2462660882@qq.com>
2021-12-25 09:21:54 +0800

    更改数据库密码

--- questionnaire/settings.py
+++ questionnaire/settings.py
@@ -80,8 +80,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'questionnaire',
         'USER': 'root',
-        'PASSWORD': 'abc123400197',
-        'HOST': '1.15.30.214',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {'isolation_level': None} #添加这一行
     }
